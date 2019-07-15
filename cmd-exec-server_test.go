31ac1f585389f141e6a6ba5286a674d0a9e71000
Hiroki Ito <hiroki110@gmail.com>
2019-07-16 06:27:45 +0900

    modified GET=>POST

--- cmd-exec-server_test.go
+++ cmd-exec-server_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,7 +13,8 @@ func TestPingRoute(t *testing.T) {
 	router := setupRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/exec", nil)
+	req, _ := http.NewRequest("POST", "/api/exec", strings.NewReader("some input"))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	router.ServeHTTP(w, req)
 
 	json := `{"result":"SOME INPUT"}`
